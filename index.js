3bec0c69638ce5f7033ab374f7dad5ad3856c214
Vincent van Wyk <vincent@fluenty.co.za>
2025-02-21 11:14:51 +0000

    Initial commit.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const request = require("request");
 
 exports.handler = function(event, context, callback) {
-  const key = "YOUR-KEY-HERE";
-  const url = "https://spreadsheets.google.com/feeds/list/"+key+"/od6/public/values?alt=json";
+  // const key = "1QTF579Jt7SFprh2ITQ6SJLwKPBXsE9yS7QmoMlct4TU";
+  // const url = "https://spreadsheets.google.com/feeds/list/"+key+"/od6/public/values?alt=json&key=AIzaSyC5BT5fHmDf2FBUTIlqLpPUdz2twCV8oms";
+  const url = "https://sheets.googleapis.com/v4/spreadsheets/1QTF579Jt7SFprh2ITQ6SJLwKPBXsE9yS7QmoMlct4TU/values/Sheet1?alt=json&key=AIzaSyC5BT5fHmDf2FBUTIlqLpPUdz2twCV8oms";
 
   request({
     json: true,
@@ -10,10 +11,14 @@ exports.handler = function(event, context, callback) {
   }, function (error, response, body) {
     if (error || response.statusCode !== 200) return
 
+    console.log(body)
+    return
+
     let parsed = body.feed.entry.map( (entry) => {
       let columns = {
         "updated": entry.updated["$t"]
       }
+      console.log('entry ', entry)
 
       // Dynamically add all relevant columns from the Sheets to the response
       Object.keys( entry ).forEach( (key) => {
